7708def8007b5818fc09da62ad2bf2ae7a6b0604
icealleymedia <icealleymedia@gmail.com>
2018-01-22 22:10:49 -0500

    staff list, staff history and staff profile sections added

--- staff-managment.php
+++ staff-managment.php
@@ -2,4 +2,16 @@
 <!-- add new staff member -->
 <button id="add-staff">Add New</button>
 <!-- staff list and online status with editing capabilities -->
+<div id="activestaffList">
+	<h3>Active Staff Members</h2>
+</div>
+<!-- display past staff members and information -->
+<div id="staffProfile">
+</div>
+<div id="staffHistory">
+	<h3>Inactive Staff Members</h3>
+</div>
+<!-- hidden add staff form that will popukate in shadow box when add staff button clicked -->
+<form id="addstaff">
+</form>
 <? require_once("includes/footer.php"); ?>
