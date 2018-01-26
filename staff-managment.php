60bb2a32ebfb833de1199477d579b6e54d43ebea
CP <MacBook@Bros-MacBook-Pro.local>
2018-01-25 21:02:19 -0800

    Staff management
    Created html search field, work in progress

--- staff-managment.php
+++ staff-managment.php
@@ -1,4 +1,12 @@
 <? require_once("includes/header.php"); ?>
+<!-- search field -->
+<div id="search">
+   <div class="lookup">
+   <h4>Find a profile</h4>
+      <input type="text" class="searchValue" placeholder="Lookup by phone or email!">
+      <button type="submit" class="searchButton"></button>
+   </div>
+</div>
 <!-- add new staff member -->
 <button id="add-staff">Add New</button>
 <!-- staff list and online status with editing capabilities -->
@@ -11,7 +19,7 @@
 <div id="staffHistory">
 	<h3>Inactive Staff Members</h3>
 </div>
-<!-- hidden add staff form that will popukate in shadow box when add staff button clicked -->
+<!-- hidden add staff form that will populate in shadow box when add staff button clicked -->
 <form id="addstaff">
 </form>
 <? require_once("includes/footer.php"); ?>
