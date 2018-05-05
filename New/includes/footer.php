ef2e28da3d98fe1a52db131f3a3830a7624b7303
icealleymedia <32878819+icealleymedia@users.noreply.github.com>
2018-05-05 14:13:41 -0400

    added debugging feature.
    added debugging feature to aid in development by displaying errors and outputing variable values only if debug is turned on.

--- New/includes/footer.php
+++ New/includes/footer.php
@@ -4,5 +4,13 @@
 		</footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 		<script src="js/appinit.js"></script>
+		<?php
+			if(APP_DEBUG == true){
+				$all_vars = get_defined_vars();
+				echo "<pre>";
+				var_dump($all_vars);
+				echo "</pre>";
+			}
+		?>
 	</body>
 </html>
--- New/includes/init.php
+++ New/includes/init.php
@@ -1,4 +1,9 @@
 <?php
+	define("APP_DEBUG", false); // change to true for application to display all errors and created variables and there values
+	if(APP_DEBUG == true){
+		ini_set('display_errors', 'On');
+		error_reporting(E_ALL);
+	}
 	set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/New' );
 	//$siteRoot = dirname(__DIR__) . '/New';
 	if(session_status() == PHP_SESSION_NONE){
